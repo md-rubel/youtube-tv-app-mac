3131d2090d30ffed2d2d21365064c4d1752568ce
MD RUBEL <hello@mdrubel.net>
2021-08-28 17:29:04 +0600

    Initial Commit

---
+++ Youtube TV/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Youtube TV
+//
+//  Created by MD RUBEL on 28/8/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
